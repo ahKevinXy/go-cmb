b66e159d189cf41df24c2d65c3e9eb578b2b8615
ahKevinXy <2502329620@qq.com>
2023-04-10 14:53:05 +0800

    1. 添加主账户支付 2. 添加交易流水 3. 添加批量支付

--- models/account.go
+++ models/account.go
@@ -28,3 +28,39 @@ type Signature struct {
 	Sigdat string `json:"sigdat"`
 	Sigtim string `json:"sigtim"`
 }
+
+// MainAccountHistoryBalanceRequest   查询概要信息
+type MainAccountHistoryBalanceRequest struct {
+	Request   MainAccountHistoryBalanceData `json:"request"`
+	Signature Signature                     `json:"signature"`
+}
+
+type MainAccountHistoryBalanceData struct {
+	Body NtqabinfyBody `json:"body,omitempty"`
+	Head Head          `json:"head"`
+}
+
+type NtqabinfyBody struct {
+	Ntqabinfy []*Ntqabinfy `json:"ntqabinfy,omitempty"`
+}
+
+type Ntqabinfy struct {
+	Accnbr string `json:"accnbr"`
+	Bbknbr string `json:"bbknbr"`
+	Bgndat string `json:"bgndat"`
+	Enddat string `json:"enddat"`
+}
+
+// AccountBankInfoRequest   获取银联号请求 todo
+type AccountBankInfoRequest struct {
+	Request   AccountBankInfoData `json:"request"`
+	Signature Signature           `json:"signature"`
+}
+
+type AccountBankInfoData struct {
+	Body AccountBankInfoBody `json:"body,omitempty"`
+	Head Head                `json:"head"`
+}
+type AccountBankInfoBody struct {
+	Fctval string `json:"fctval,omitempty"`
+}
